1889d0eee31c016640e37718ec572d2d16b0c332
Tyler Faulkner <tylersfaulkner@gmail.com>
2022-03-30 14:15:34 -0500

    Added file to create the AffectNet Dataframe

---
+++ PreProcessing Files/create_dataframe.py
@@ -0,0 +1,26 @@
+from os import listdir
+import re
+import numpy as np
+import pandas as pd
+
+train_path = "/data/datasets/affectNet/train_set/"
+path = "/data/datasets/affectNet/train_set/annotations/"
+img_path = "/data/datasets/affectNet/train_set/images/"
+files = listdir(path)
+exp = np.array(list(filter(lambda c: 'exp' in c, files)))
+# print(list(exp))
+nums = re.compile('\d*')
+
+y_train = np.empty([len(exp), 2], dtype=np.int32)
+
+df = pd.DataFrame(columns=['path', 'label'])
+
+for i in range((exp.shape[0])):
+    file = exp[i]
+    num = nums.match(file)[0]
+    array = np.load(path + file)
+    # print(array)
+    df = df.append({'path': img_path + str(num) + ".jpg", "label": array}, ignore_index=True)
+    print(df)
+
+df.to_csv(train_path, index=False)
