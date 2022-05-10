638eda9812315b40c9be4ad38d614140299c1a8f
Nelson <nelsonni@msoe.edu>
2022-05-10 14:22:54 -0500

    Update confusionMatrixGen to include useful comments

--- TrainModelTemplate/EfficientNet/confusionMatrixGen.py
+++ TrainModelTemplate/EfficientNet/confusionMatrixGen.py
@@ -1,4 +1,4 @@
-### Use the below srun command on rosie to run this script
+### Use the below srun command on Rosie to run this script
 ### srun --partition=teaching --gpus=1 --cpus-per-gpu=2 singularity exec --nv -B /data:/data /data/containers/msoe-tensorflow-20.07-tf2-py3.sif /usr/local/bin/nvidia_entrypoint.sh python confusionMatrixGen.py
 
 
@@ -8,8 +8,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import os
-import sklearn
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
@@ -18,20 +17,9 @@ batch_size = 1
 input_shape = (224,224)
 target_size=(224,224)
 
-# test_dir = '/home/nelsonni/data/data/cs3310/Huupe/val_class'
-
 test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
 
-# validation_generator=test_datagen.flow_from_directory(test_dir,
-#                                             class_mode="categorical",
-#                                             target_size=input_shape,
-#                                             batch_size=batch_size,
-#                                             shuffle=False)
-
-# num_test_imgs = 0
-# for root, dirs, files in os.walk(test_dir):
-#     num_test_imgs += len(files)
-
+# Use which ever .csv file contains your validation images
 valdf = pd.read_csv('/data/cs3310/Huupe/affect_full_val.csv')
 
 validation_generator = test_datagen.flow_from_dataframe(
@@ -39,10 +27,10 @@ validation_generator = test_datagen.flow_from_dataframe(
     x_col='path',
     y_col='label',
     batch_size=batch_size,
-    class_mode="categorical",
-    target_size=target_size)
+    target_size=target_size,
+    shuffle=False)
 
-
+# Use which ever model is desired to be tested
 model=load_model('ENetB0_E45_B64_ImageNet_5149_valAcc.h5')
 
 test_pred = model.predict(validation_generator, batch_size=batch_size)
@@ -67,4 +55,3 @@ ax.set_xlabel('\nPredicted Values')
 ax.set_ylabel('Actual Values ')
 
 plt.savefig('ConfusionMatrix.png')
-
